7f703be96466a5e874ade44099de1d6945e314ef
Petrus <petrusheya@gmail.com>
2024-05-24 14:06:42 +0200

    Added body middleware

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import fetch from 'node-fetch'
 import express from 'express'
 import cors from 'cors'
 
-const app = express()
 const PORT = 8080
+const app = express()
 
-app.listen(PORT, ()=> {console.log(`Server listening on port ${PORT}`)})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 app.use(cors({
   origin: '*'
 }))
+
+app.listen(PORT, ()=> {console.log(`Server listening on port ${PORT}`)})
 
 app.get('/mockups', async (req,res)=>{
     console.log('Receved Request')
@@ -65,7 +68,7 @@ app.get('/mockups', async (req,res)=>{
 })
 
 app.get('/printful-mockups', async (req,res)=>{
-    // const body = req.body
+    const body = req.body
     console.log('Received Request')
     const fetchOptions = {
         method: "POST",
@@ -75,37 +78,30 @@ app.get('/printful-mockups', async (req,res)=>{
           'X-PF-Store-Id': '8893037'
         },
         body: JSON.stringify({
-          "variant_ids": [
-            4012,
-            4013,
-            4014,
-            4017,
-            4018,
-            4019
-          ],
+          "variant_ids": [17057],
           "format": "jpg",
           "files": [
             {
               "placement": "front",
-              "image_url": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=600g",
+              "image_url": `${body.src}`,
               "position": {
-                "area_width": 1800,
-                "area_height": 2400,
-                "width": 1800,
-                "height": 1800,
-                "top": 300,
+                "area_width": 2325,
+                "area_height": 2940,
+                "width": 2325,
+                "height": 2325,
+                "top": 307,
                 "left": 0
               }
             },
             {
               "placement": "back",
-              "image_url": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=600",
+              "image_url": `${body.src}`,
               "position": {
-                "area_width": 1800,
-                "area_height": 2400,
-                "width": 1800,
-                "height": 1800,
-                "top": 300,
+                "area_width": 2325,
+                "area_height": 2940,
+                "width": 2325,
+                "height": 2325,
+                "top": 307,
                 "left": 0
               }
             }
@@ -122,20 +118,24 @@ app.get('/printful-mockups', async (req,res)=>{
     let isProcessed = false
     let generationResult;
     try {
-        const response = await fetch('https://api.printful.com/mockup-generator/create-task/71', fetchOptions)
+        const response = await fetch(`https://api.printful.com/mockup-generator/create-task/${body.prodId}`, fetchOptions)
+
         if(!response.ok){
             const responseError = await response.json()
             console.log(responseError)
-            throw new Error('Error Creating Mockup')
+            throw new Error('Error Creating Mockup, Try Again')
         }
+
         const responseObject = await response.json()
         console.log('Heres the task_key:', responseObject.result.task_key)
         const taskKey = responseObject.result.task_key
+
         await new Promise((resolve,_)=>{
             setTimeout(()=>{
                 resolve()
             }, 30000)
         })
+
         while(!isProcessed){
             const pollResponse = await fetch(`https://api.printful.com/mockup-generator/task?task_key=${taskKey}`,pollingOptions)
             if(!pollResponse.ok){
@@ -156,8 +156,10 @@ app.get('/printful-mockups', async (req,res)=>{
                 })
             }
         }
+
         console.log(generationResult)
         res.json({result: generationResult})
+
     } catch (error) {
         console.error(error)
         res.json({error}).status(500)
@@ -186,4 +188,4 @@ app.get('/store-id', async(req,res)=>{
     } catch (error) {
         console.error('Error fetching store information:', error);
     }
-})
+})  
